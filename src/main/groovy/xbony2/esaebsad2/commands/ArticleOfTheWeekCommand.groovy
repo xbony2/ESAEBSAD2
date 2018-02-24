2e7bd943cf0af0c3f9f787d11dac557efe0e30d4
Eric Schneider <xbony22@gmail.com>
2018-02-24 12:45:01 -0500

    extremely advanced algorithm

--- src/main/groovy/xbony2/esaebsad2/commands/ArticleOfTheWeekCommand.groovy
+++ src/main/groovy/xbony2/esaebsad2/commands/ArticleOfTheWeekCommand.groovy
@@ -5,7 +5,7 @@ import de.btobastian.sdcf4j.CommandExecutor
 import xbony2.esaebsad2.ESAEBSAD2
 
 class ArticleOfTheWeekCommand implements CommandExecutor {
-	@Command(aliases = ["!articleoftheweek"], description = "The article of the week command decided the article of the week using an extremely advanced algorithim.")
+	@Command(aliases = ["!articleoftheweek"], description = "The article of the week command decided the article of the week using an extremely advanced algorithm.")
 	onCommand(){
 		def page = ESAEBSAD2.wiki.getRandomPages(1)[0]
 		
